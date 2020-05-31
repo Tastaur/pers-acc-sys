03295ec693f1a37a6ebdc7103ec2642e496db1dc
Mark <Trrrrex93@gmail.com>
2020-05-31 15:41:37 +0400

    Updates

---
+++ precache-manifest.511c7acc7ea2b81e187205b58bea62eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999facec781d72750e7f3ee9287ed555",
+    "url": "/pers-acc-sys/index.html"
+  },
+  {
+    "revision": "bbccd89a28d9dba9cc71",
+    "url": "/pers-acc-sys/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4860625b786ea666f550",
+    "url": "/pers-acc-sys/static/css/main.f9728130.chunk.css"
+  },
+  {
+    "revision": "bbccd89a28d9dba9cc71",
+    "url": "/pers-acc-sys/static/js/2.c312829b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pers-acc-sys/static/js/2.c312829b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4860625b786ea666f550",
+    "url": "/pers-acc-sys/static/js/main.8e4bad13.chunk.js"
+  },
+  {
+    "revision": "a6eafbc4f09dd95785f8",
+    "url": "/pers-acc-sys/static/js/runtime-main.1a65faf2.js"
+  }
+]);
